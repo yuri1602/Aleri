d8805f4baa537c69beba563bc4f3a9a088adf8e1
Юрий-Александър Методиев <62445997+yuri1602@users.noreply.github.com>
2025-02-27 22:27:32 +0200

    Fixed compatibility flag issue

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,15 +4,15 @@ import Image from "next/image"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-32 items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
+      <div className="container flex h-20 items-center justify-between">
+        <Link href="/" className="flex items-center space-x-2 -my-8">
           <Image
              src="/placeholder-logo1.png" 
              alt="Aleri Group Logo" 
-             width={1500} 
-             height={375} 
+             width={2000} 
+             height={500} 
              priority
-             className="h-28 w-auto"
+             className="h-36 w-auto"
           />
         </Link>
         <nav className="ml-auto flex gap-6">
